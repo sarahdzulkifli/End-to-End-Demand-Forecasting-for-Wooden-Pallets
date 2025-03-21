817b0fe79684475551f4fdb51b4ab2cfbd570a81
sarahdzulkifli <sarahdzulkifli@gmail.com>
2025-03-21 08:29:13 +0800

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from DemandForecasting_WoodenPallets import logger
+from DemandForecasting_WoodenPallets.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("Welcome to custom logging")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
