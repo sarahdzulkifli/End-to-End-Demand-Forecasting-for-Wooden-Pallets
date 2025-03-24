83576a9376171fbcd5d13bccaef6a04ec8ce08aa
sarahdzulkifli <sarahdzulkifli@gmail.com>
2025-03-24 21:41:03 +0800

    data validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from DemandForecasting_WoodenPallets import logger
 from DemandForecasting_WoodenPallets.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from DemandForecasting_WoodenPallets.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -7,6 +8,19 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
